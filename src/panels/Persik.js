72d150508a835dbfc615c00579b5f609b534db99
Your Name <you@example.com>
2020-09-26 11:47:30 +0300

    asm

---
+++ src/panels/Persik.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { platform, IOS } from '@vkontakte/vkui';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
+import Cell from "@vkontakte/vkui/dist/components/Cell/Cell";
+import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+
+import persik from '../img/persik.png';
+import './Persik.css';
+
+const osName = platform();
+
+const Persik = props => (
+	<Panel id={props.id}>
+		<PanelHeader
+			left={<PanelHeaderButton onClick={props.go} data-to="home">
+				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
+			</PanelHeaderButton>}
+		>
+			Hello§×]
+		</PanelHeader>
+		<img className="Persik" src={persik} alt="Persik The Cat"/>
+		<Cell>
+			<p className="None"><big><b>ПУСТО :(</b></big></p>
+		</Cell>
+	</Panel>
+);
+
+Persik.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+};
+
+export default Persik;
